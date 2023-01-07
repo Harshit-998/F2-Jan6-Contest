6832774dbc93ec38f857217f04d0f62a18f43ca3
Harshit-998 <harshityadav0767@gmail.com>
2023-01-07 21:22:07 +0530

    contest

--- script.js
+++ script.js
@@ -6,22 +6,53 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
-function PrintDeveloperswithMap() {
+function PrintDeveloperswithMap(element) {
   //Write your code here
+  arr.map(PrintDevelopersUsingMap);
+
+  function PrintDevelopersUsingMap(item) {
+    if (item.profession === "developer") {
+      console.log(item);
+    }
+  }
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(PrintDevelopersUsingForEach);
+
+  function PrintDevelopersUsingForEach(item) {
+    if (item.profession === "developer") {
+      console.log(item);
+    }
+  }
 }
 
 function addData() {
   //Write your code here
+  let newItem = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newItem);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  let removedArr = arr.filter(function (item) {
+    if (item.profession !== "admin") {
+      return item;
+    }
+  });
+  console.log(removedArr);
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+    { id: 5, name: "Henry Hill", age: "23", profession: "Gangster" },
+    { id: 6, name: "Vito Corleone", age: "35", profession: "Godfather" },
+    { id: 7, name: "Sonny", age: "30", profession: "Mobster" },
+  ];
+
+  let concatArr = arr.concat(arr2);
+  console.log(concatArr);
 }
